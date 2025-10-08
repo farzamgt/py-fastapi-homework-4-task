b97f1d97c3462974e5a87006ba0e854322ea5332
Farzam <farzamgt@gmail.com>
2025-10-08 20:17:07 +0300

    fix: update profile schema async def

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -58,7 +58,7 @@ class ProfileRequestSchema(BaseModel):
 
     @field_validator("avatar")
     @classmethod
-    async def validate_avatar(cls, avatar: UploadFile) -> UploadFile:
+    async def validate_avatar(self) -> UploadFile:
         try:
             await s3_storage.upload_file(avatar)
         except S3FileUploadError:
